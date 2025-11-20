52138dc34f627d9b4de125b58e64d42ab6ebb5d5
Sakata <pichugov-sasha@mail.ru>
2025-11-20 17:50:03 +0300

    Залил 4 лабу

--- lw4/lw4.cpp
+++ lw4/lw4.cpp
@@ -47,13 +47,176 @@ protected:
     }
 };
 
-optional<Args> ParseArgs(int argc, char** argv);
-DWORD WINAPI BlurBitmap(CONST LPVOID lpParam);
-Threads CreateThreads(size_t count, function<ProcessBitmapInfo* (int)> dataCreatorFn);
-void SetCoresLimit(size_t limit);
-void SetNumbersDecimalPoint();
-GetEllapsedTimeFn StartTimer();
-int MapNumberToPriority(int number);
+static Pixel Average(vector<optional<Pixel>> const& v)
+{
+    auto const count = v.size();
+    int sumR = 0;
+    int sumG = 0;
+    int sumB = 0;
+    int pixelsCount = 0;
+
+    for (auto const& pixel : v)
+    {
+        if (!pixel)
+        {
+            continue;
+        }
+        pixelsCount++;
+        sumR += pixel->r;
+        sumG += pixel->g;
+        sumB += pixel->b;
+    }
+
+    return Pixel(sumR / pixelsCount, sumG / pixelsCount, sumB / pixelsCount);
+}
+
+static Pixel GetAverageColor(Bitmap const& img, int x, int y)
+{
+    vector<optional<Pixel>> pixels = {
+        img.get(x, y),
+        img.get(x, y - 1),
+        img.get(x, y + 1),
+        img.get(x - 1, y),
+        img.get(x - 1, y - 1),
+        img.get(x - 1, y + 1),
+        img.get(x + 1, y),
+        img.get(x + 1, y - 1),
+        img.get(x + 1, y + 1),
+    };
+
+    return Average(pixels);
+}
+
+DWORD WINAPI BlurBitmap(CONST LPVOID lpParam)
+{
+    auto data = reinterpret_cast<ProcessBitmapInfo*>(lpParam);
+
+    ofstream output;
+    output.open("thread" + to_string(data->lineNumber) + ".txt");
+
+    unsigned startY = data->lineNumber * data->lineHeight;
+
+    auto image = data->image;
+    unsigned imageWidth = image->width();
+
+    for (unsigned i = 0; i < 21; i++)
+    {
+        for (unsigned y = startY; y < startY + data->lineHeight; ++y)
+        {
+            for (unsigned x = 0; x < imageWidth; ++x)
+            {
+                image->set(x, y, GetAverageColor(*image, x, y));
+            }
+        }
+        output << data->getTime().count() << endl;
+    }
+
+    output.close();
+
+    delete data;
+    ExitThread(0);
+}
+
+static optional<Args> ParseArgs(int argc, char** argv)
+{
+    constexpr int MIN_ARGS_COUNT = 5;
+
+    if (argc < MIN_ARGS_COUNT)
+    {
+        return nullopt;
+    }
+
+    Args result;
+
+    result.inputFileName = argv[1];
+    result.outputFileName = argv[2];
+
+    try
+    {
+        result.coresCount = stoi(argv[3]);
+        result.threadsCount = stoi(argv[4]);
+
+
+        if (argc < MIN_ARGS_COUNT + result.threadsCount)
+        {
+            return nullopt;
+        }
+
+        for (int i = 0; i < result.threadsCount; i++)
+        {
+            result.priority.push_back(stoi(argv[MIN_ARGS_COUNT + i]));
+        }
+    }
+    catch (...)
+    {
+        return nullopt;
+    }
+
+    return result;
+}
+
+static Threads CreateThreads(size_t count, function<ProcessBitmapInfo* (int)> dataCreatorFn)
+{
+    auto threads = make_unique<HANDLE[]>(count);
+
+    for (unsigned i = 0; i < count; i++)
+    {
+        threads[i] = CreateThread(
+            NULL, 0, &BlurBitmap, dataCreatorFn(i), CREATE_SUSPENDED, NULL
+        );
+    }
+
+    return threads;
+}
+
+static void SetNumbersDecimalPoint()
+{
+    locale comma_locale(locale(), new comma_numpunct());
+    cout.imbue(comma_locale);
+}
+
+static void SetCoresLimit(size_t limit)
+{
+    SYSTEM_INFO sysinfo;
+    GetSystemInfo(&sysinfo);
+    size_t maxCoresCount = sysinfo.dwNumberOfProcessors;
+
+    if (limit > maxCoresCount)
+    {
+        cout << "Max cores count is " << maxCoresCount << endl;
+        limit = maxCoresCount;
+    }
+
+    auto procHandle = GetCurrentProcess();
+    DWORD_PTR mask = static_cast<DWORD_PTR>((pow(2, maxCoresCount) - 1) / pow(2, maxCoresCount - limit));
+
+    SetProcessAffinityMask(procHandle, mask);
+}
+
+static GetEllapsedTimeFn StartTimer()
+{
+    chrono::steady_clock::time_point start = chrono::steady_clock::now();
+
+    return [start]()
+        {
+            return chrono::steady_clock::now() - start;
+        };
+}
+
+static int MapNumberToPriority(int number)
+{
+    switch (number) {
+    case 0:
+        return THREAD_PRIORITY_BELOW_NORMAL;
+    case 1:
+        return THREAD_PRIORITY_NORMAL;
+    case 2:
+        return THREAD_PRIORITY_ABOVE_NORMAL;
+    default:
+        cout << "unknown priority value " << number << ", set to normal";
+        return THREAD_PRIORITY_NORMAL;
+    }
+}
 
 int main(const int argc, char** argv)
 {
@@ -105,175 +268,4 @@ int main(const int argc, char** argv)
     }
 
     return 0;
-}
-
-optional<Args> ParseArgs(int argc, char** argv)
-{
-    constexpr int MIN_ARGS_COUNT = 5;
-
-    if (argc < MIN_ARGS_COUNT)
-    {
-        return nullopt;
-    }
-
-    Args result;
-
-    result.inputFileName = argv[1];
-    result.outputFileName = argv[2];
-
-    try
-    {
-        result.coresCount = stoi(argv[3]);
-        result.threadsCount = stoi(argv[4]);
-
-
-        if (argc < MIN_ARGS_COUNT + result.threadsCount)
-        {
-            return nullopt;
-        }
-
-        for (int i = 0; i < result.threadsCount; i++)
-        {
-            result.priority.push_back(stoi(argv[MIN_ARGS_COUNT + i]));
-        }
-    }
-    catch (...)
-    {
-        return nullopt;
-    }
-
-    return result;
-}
-
-Threads CreateThreads(size_t count, function<ProcessBitmapInfo* (int)> dataCreatorFn)
-{
-    auto threads = make_unique<HANDLE[]>(count);
-
-    for (unsigned i = 0; i < count; i++)
-    {
-        threads[i] = CreateThread(
-            NULL, 0, &BlurBitmap, dataCreatorFn(i), CREATE_SUSPENDED, NULL
-        );
-    }
-
-    return threads;
-}
-
-Pixel Average(vector<optional<Pixel>> const& v)
-{
-    auto const count = v.size();
-    int sumR = 0;
-    int sumG = 0;
-    int sumB = 0;
-    int pixelsCount = 0;
-
-    for (auto const& pixel : v)
-    {
-        if (!pixel)
-        {
-            continue;
-        }
-        pixelsCount++;
-        sumR += pixel->r;
-        sumG += pixel->g;
-        sumB += pixel->b;
-    }
-
-    return Pixel(sumR / pixelsCount, sumG / pixelsCount, sumB / pixelsCount);
-}
-
-Pixel GetAverageColor(Bitmap const& img, int x, int y)
-{
-    vector<optional<Pixel>> pixels = {
-        img.get(x, y),
-        img.get(x, y - 1),
-        img.get(x, y + 1),
-        img.get(x - 1, y),
-        img.get(x - 1, y - 1),
-        img.get(x - 1, y + 1),
-        img.get(x + 1, y),
-        img.get(x + 1, y - 1),
-        img.get(x + 1, y + 1),
-    };
-
-    return Average(pixels);
-}
-
-DWORD WINAPI BlurBitmap(CONST LPVOID lpParam)
-{
-    auto data = reinterpret_cast<ProcessBitmapInfo*>(lpParam);
-
-    ofstream output;
-    output.open("thread" + to_string(data->lineNumber) + ".txt");
-
-    unsigned startY = data->lineNumber * data->lineHeight;
-
-    auto image = data->image;
-    unsigned imageWidth = image->width();
-
-    for (unsigned i = 0; i < 21; i++)
-    {
-        for (unsigned y = startY; y < startY + data->lineHeight; ++y)
-        {
-            for (unsigned x = 0; x < imageWidth; ++x)
-            {
-                image->set(x, y, GetAverageColor(*image, x, y));
-            }
-        }
-        output << data->getTime().count() << endl;
-    }
-
-    output.close();
-
-    delete data;
-    ExitThread(0);
-}
-
-void SetNumbersDecimalPoint()
-{
-    locale comma_locale(locale(), new comma_numpunct());
-    cout.imbue(comma_locale);
-}
-
-void SetCoresLimit(size_t limit)
-{
-    SYSTEM_INFO sysinfo;
-    GetSystemInfo(&sysinfo);
-    size_t maxCoresCount = sysinfo.dwNumberOfProcessors;
-
-    if (limit > maxCoresCount)
-    {
-        cout << "Max cores count is " << maxCoresCount << endl;
-        limit = maxCoresCount;
-    }
-
-    auto procHandle = GetCurrentProcess();
-    DWORD_PTR mask = static_cast<DWORD_PTR>((pow(2, maxCoresCount) - 1) / pow(2, maxCoresCount - limit));
-
-    SetProcessAffinityMask(procHandle, mask);
-}
-
-GetEllapsedTimeFn StartTimer()
-{
-    chrono::steady_clock::time_point start = chrono::steady_clock::now();
-
-    return [start]()
-        {
-            return chrono::steady_clock::now() - start;
-        };
-}
-
-int MapNumberToPriority(int number)
-{
-    switch (number) {
-    case 0:
-        return THREAD_PRIORITY_BELOW_NORMAL;
-    case 1:
-        return THREAD_PRIORITY_NORMAL;
-    case 2:
-        return THREAD_PRIORITY_ABOVE_NORMAL;
-    default:
-        cout << "unknown priority value " << number << ", set to normal";
-        return THREAD_PRIORITY_NORMAL;
-    }
 }
